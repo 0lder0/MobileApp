bf189067db5d120eb35d08635b9f160d65f9b6d5
0lder0 <94623139+0lder0@users.noreply.github.com>
2024-02-09 11:40:21 +0200

    Create Splash screen

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#4F63AC",
+        paddingHorizontal: 8,
+        paddingVertical: 20,
+        width: "100%",
+        borderRadius: 8,
+    },
+    title: {
+        color: "white",
+        textAlign: "center",
+        fontSize: 16,
+        fontWeight: "bold"
+    }
+});
