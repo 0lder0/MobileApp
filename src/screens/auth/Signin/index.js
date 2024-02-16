3e178aa6c5136ef697adfd4de3f9f617ef0ed52b
0lder0 <lilii-ann.older@khk.ee>
2024-02-16 12:19:20 +0200

    Create authorization screens

--- src/screens/auth/Signin/index.js
+++ src/screens/auth/Signin/index.js
@@ -1,35 +1,28 @@
-import React, { useState} from "react";
+import React from "react";
 import { View, Text } from "react-native";
 import AuthHeader from "../../../components/AuthHeader";
 import { styles } from "./styles";
 import Input from "../../../components/Input";
-import Checkbox from "../../../components/Checkbox";
 import Button from "../../../components/Button";
 import Separator from "../../../components/Separator";
 import GoogleLogin from "../../../components/GoogleLogin";
 
 
-const Signup = () => {
-    const [checked, setChecked] = useState(false);
+const Signin = () => {
 
     return (
         <View style={styles.container}>
-            <AuthHeader title="Sign Up"/>
-            <Input label="Name" placeholder="John Doe"/>
+            <AuthHeader title="Sign In"/>
             <Input label="Email" placeholder="example@gmail.com"/>
             <Input isPassword label="Password" placeholder="********"/>
-            <View style={styles.agreeRow}>
-                <Checkbox checked={checked} onCheck={setChecked}/>
-                <Text style={styles.agreeText}>I agree with <Text style={styles.agreeTextBold}>Terms</Text> & <Text style={styles.agreeTextBold}>Privacy</Text></Text>
-            </View>
             <Button title="Sign In" style={styles.button}/>
             <Separator text="or Sign Up with"/>
             <GoogleLogin/>
-            <Text style={styles.footerText}>Already have an account?
-             <Text style={styles.footerLink}> Sign In</Text>
+            <Text style={styles.footerText}>Don't have an account?
+             <Text style={styles.footerLink}> Sign Up</Text>
             </Text>
         </View>
     );
 }
 
-export default Signup;
+export default Signin;
